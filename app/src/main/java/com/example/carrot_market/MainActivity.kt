325bfd5e5d3c59f419d1714ed395ae2eb4439b02
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-04-16 19:19:32 +0900

    Revert "[Feat] 메인페이지 기능 구현"

---
+++ app/src/main/java/com/example/carrot_market/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.carrot_market
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.carrot_market.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
