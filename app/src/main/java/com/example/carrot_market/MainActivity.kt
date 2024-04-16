3bcf9311c9dc3b406dd8bdef12fc8b7147f37c7e
rlaxodud214 <rlaxodud214@naver.com>
2024-04-16 14:29:28 +0900

    feat: 메인페이지 및 item_post UI 구현

--- app/src/main/java/com/example/carrot_market/MainActivity.kt
+++ app/src/main/java/com/example/carrot_market/MainActivity.kt
@@ -19,5 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
     }
 }
